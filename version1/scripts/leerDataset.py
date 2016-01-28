fefec2c4a099948a3d324281f9349a0f788075ee
ynunezaguirre <yamil.nunez@gmail.com>
2016-01-28 12:15:06 -0500

    Uniendo2

---
+++ version1/scripts/leerDataset.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import hashlib
+import sys
+
+
+for line in sys.stdin:
+	
+	print line
---
+++ version1/scripts/md5sum.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import hashlib
+import sys
+
+m = hashlib.md5()
+for line in sys.stdin:
+     m.update(line)
+     print m.hexdigest()
