6723fe6d837c4201ac5f6cf75522d34068b853a9
ynunezaguirre <yamil.nunez@gmail.com>
2016-01-28 12:34:37 -0500

    update

--- version1/scripts/leerDataset.py
+++ version1/scripts/leerDataset.py
@@ -3,6 +3,26 @@ import hashlib
 import sys
 
 
+#inicializo la suma para los dataset pares e impares
+sumaPares= 0;
+sumaImpares=0;
 for line in sys.stdin:
-	
-	print line
+	#obtengo el numero de data set 
+	numeroDataset = line.split(":")
+	numeroDataset = numeroDataset[0];
+	#limpio los espacios en blanco
+	lineaNumeros = numeroDataset[1].strip();
+	#obtengo los numeros dentro de cada dataset
+	arrayNumeros = lineaNumeros.split(" ")
+	sumaInternaDataset = 0;
+	if len(arrayNumeros)>0:
+		for numero in arrayNumeros:
+			sumaInternaDataset=sumaInternaDataset+numero;
+	#compruebo si es impar o par el dataset
+	if numeroDataset%2==0:
+		sumaPares = sumaPares + sumaInternaDataset	
+	else:
+		sumaImpares = sumaImpares + sumaInternaDataset	
+
+print "La suma de los dataset pares es: "+sumaPares+"\n"
+print "La suma de los dataset impares es: "+sumaImpares+"\n"
