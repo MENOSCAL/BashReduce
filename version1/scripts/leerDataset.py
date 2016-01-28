6e2b2294b30a03937c082ffb78c3dacf7db204c5
ynunezaguirre <yamil.nunez@gmail.com>
2016-01-28 12:56:29 -0500

    fixLeerDatasetRemovePrint

--- version1/scripts/leerDataset.py
+++ version1/scripts/leerDataset.py
@@ -9,26 +9,22 @@ sumaImpares=0;
 for line in sys.stdin:
 	#obtengo el numero de data set 
 	arrayDataset = line.split(":")
-	print arrayDataset
 	numeroDataset = arrayDataset[0];
 	#limpio los espacios en blanco y eliminar el retorno de carro
 	lineaNumeros = arrayDataset[1].strip();
 	lineaNumeros = lineaNumeros.replace("\n","");
-	print lineaNumeros
 	#obtengo los numeros dentro de cada dataset
 	arrayNumeros = lineaNumeros.split(" ")
-	print arrayNumeros
 	sumaInternaDataset = 0;
 	if len(arrayNumeros)>0:
 		for numero in arrayNumeros:
-			print numero
 			sumaInternaDataset=sumaInternaDataset+int(numero);
-			print sumaInternaDataset
 	#compruebo si es impar o par el dataset
+
 	if int(numeroDataset)%2==0:
 		sumaPares = sumaPares + sumaInternaDataset	
 	else:
 		sumaImpares = sumaImpares + sumaInternaDataset	
 
-print "La suma de los dataset pares es: "+sumaPares+"\n"
-print "La suma de los dataset impares es: "+sumaImpares+"\n"
+print "La suma de los dataset pares es: "+unicode(sumaPares)+"\n"
+print "La suma de los dataset impares es: "+unicode(sumaImpares)
